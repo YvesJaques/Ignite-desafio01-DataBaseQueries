b5e4c1340fd1947e4362eb74cc97a633a78ef1f6
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-19 08:55:52 -0300

    Implementacao das funcoes

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,21 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne(user_id,{ relations: ["games"] });
+        
+    return <User>user; 
     // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM USERS ORDER BY FIRST_NAME ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT USERS.* FROM USERS WHERE LOWER(USERS.FIRST_NAME) = LOWER('${first_name}')
+                                  AND LOWER(USERS.LAST_NAME) = LOWER('${last_name}')`); // Complete usando raw query
   }
 }
